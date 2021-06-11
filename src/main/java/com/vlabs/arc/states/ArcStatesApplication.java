bbb6bfdad8eb295fd8bc71cb5192b8b57707dc52
vidhirkaup <vidhir.kaup@gmail.com>
2021-06-10 20:25:00 -0400

    Initial commit

---
+++ src/main/java/com/vlabs/arc/states/ArcStatesApplication.java
@@ -0,0 +1,13 @@
+package com.vlabs.arc.states;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcStatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlabs/arc/states/ArcStatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlabs.arc.states;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcStatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
